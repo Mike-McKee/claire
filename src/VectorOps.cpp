32a8fb669eb069b413ac22b70ebffe1b33c7b61a
Mike McKee <mike@dorkydata.com>
2025-10-31 22:32:03 -0400

    Fixed funcs to use operator [] | added VSub, SMult, and DotProd

--- src/VectorOps.cpp
+++ src/VectorOps.cpp
@@ -7,23 +7,21 @@ namespace la {
 
 void PrintVector(const Vector& v) {
     const auto n = v.size();
-    const auto& d = v.data();
     std::cout << "[";
     for (std::size_t i = 0; i < n; ++i) {
         if (i == n - 1) {
-            std::cout << d[i] << "]";
+            std::cout << v[i] << "]";
         }
         else {
-            std::cout << d[i] << ",";
+            std::cout << v[i] << ",";
         }
     }
 }
 
 bool isZeroVector(const Vector& v) {
-    const auto& d = v.data();
     const auto& s = v.size();
     for (std::size_t i = 0; i < s; ++i) {
-        if (d[i] != 0) {
+        if (v[i] != 0) {
             return false;
         }
     }
@@ -39,20 +37,50 @@ bool isSameDimension(const Vector& v1, const Vector& v2) {
     }
 }
 
-Vector VectorAdddition(const Vector& v1, const Vector& v2) {
+Vector VectorAddition(const Vector& v1, const Vector& v2) {
     if (!isSameDimension(v1,v2)) {
         throw std::invalid_argument("Cannot add vectors with different dimensions");
     }
-    const auto& a = v1.data();
-    const auto& b = v2.data();
     std::vector<double> va;
-    va.reserve(5);
+    va.reserve(v1.size());
 
-    for (std::size_t i = 0; i < a.size(); ++i) {
-        va.push_back(a[i] + b[i]);
+    for (std::size_t i = 0; i < v1.size(); ++i) {
+        va.push_back(v1[i] + v2[i]);
     }
 
     return Vector(va);
 }
+
+Vector VectorSubtraction(const Vector& v1, const Vector& v2) {
+    if (!isSameDimension(v1,v2)) {
+        throw std::invalid_argument("Cannot subtract vectors with different dimensions");
+    }
+    Vector v2Neg = ScalarMultiplication(v2,-1);
+    return VectorAddition(v1,v2Neg);
+}
+
+Vector ScalarMultiplication(const Vector& v, double k) {
+    std::vector<double> sm;
+    sm.reserve(v.size());
+    for (std::size_t i = 0; i < v.size(); ++i) {
+        sm.push_back(v[i] * k);
+    }
+
+    return Vector(sm);
+}
+
+double DotProduct(const Vector& v1, const Vector& v2) {
+    if (!isSameDimension(v1,v2)) {
+        throw std::invalid_argument("Cannot find dot product of vectors with different dimensions");
+    }
+    double dot = 0;
+    for (std::size_t i = 0; i < v1.size(); ++i) {
+        dot += v1[i] * v2[i];
+    }
+    return dot;
+}
+
+// TODO: BELOW
+// Vector CrossProduct(const Vector& v1, const Vector& v2);
 
 }
