79e0ee23232d7b4f73feafd8bc7170998dc467d6
Mike McKee <mike@dorkydata.com>
2025-11-06 21:05:04 -0500

    Testing Vector class methods

--- test/VectorTest.cpp
+++ test/VectorTest.cpp
@@ -1,17 +1,45 @@
 #include <iostream>
 #include "claire/la/Vector.h"
+#include "claire/la/VectorOps.h"
+
+// To Compile: g++ .\VectorTest.cpp ..\src\Vector.cpp ..\src\VectorOps.cpp ..\src\Matrix.cpp -I..\include\  -std=c++20 -o VectorTest
 
 int main() {
 
-    la::Vector v({1,2,3,4});
+    // Here we'll test all Vector class methods
 
-    std::cout << "Test non-empty: Vector Size = " << v.size() << std::endl;
-    std::cout << "Test non-empty: Is Vector empty = " << std::boolalpha << v.isEmpty() << std::endl;
+    // Explicit vector constructor
+    la::Vector v({1.0,2.0,4.0,5.0,6.3});
+
+    // Create n-dimension vector with same value for each entry
+    la::Vector w(5,2.0);
+
+    // Explicit vector constructor with different initialization method
+    la::Vector u = la::Vector(5,4.0);
+
+    std::cout << "v = ";
+    la::PrintVector(v);
+    std::cout << "\n";
+
+    std::cout << "w = ";
+    la::PrintVector(w);
+    std::cout << "\n";
+
+    std::cout << "u = ";
+    la::PrintVector(u);
+    std::cout << "\n";
     
-    la::Vector empty({});
+    // Test size()
+    std::cout << "v size = " << v.size() << "\n";
+    std::cout << "w size = " << w.size() << "\n";
+    std::cout << "u size = " << u.size() << "\n";
 
-    std::cout << "Test empty: Vector Size = " << empty.size() << std::endl;
-    std::cout << "Test empty: Is Vector empty = " << std::boolalpha << empty.isEmpty() << std::endl;
+    la::Vector x(0,0.0);
+    std::cout << "is v empty = " << v.isEmpty() << "\n";
+    std::cout << "is x empty = " << x.isEmpty() << "\n";
 
+    std::cout << "v[0] = " << v[0] << "\n";
+    std::cout << "w[3] = " << w[0] << "\n";
+    std::cout << "u[2] = " << u[0] << "\n";
     return 0;
 }
