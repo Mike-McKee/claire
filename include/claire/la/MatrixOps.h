9817009a033d11a1dbc247b63753c139d85475e8
Mike McKee <mike@dorkydata.com>
2025-11-09 22:28:19 -0500

    Added AugmentedMatrix(), AugmentedMatrixSplit(), and AmSide enum

--- include/claire/la/MatrixOps.h
+++ include/claire/la/MatrixOps.h
@@ -13,7 +13,7 @@ Current method of creating multiple copy-vector objects within functions can bec
 namespace la {
 
     void PrintMatrix(const Matrix& m);
-
+    
     Matrix MatrixAddition(const Matrix& m1, const Matrix& m2);
     Matrix MatrixSubtraction(const Matrix& m1, const Matrix& m2);
     Matrix ScalarMultiplication(const Matrix& m, double k);
@@ -21,16 +21,19 @@ namespace la {
     double Determinant2x2(const Matrix& m);
     Matrix MinorMatrix(const Matrix& m, const std::set<Matrix::size_type>& r, const std::set<Matrix::size_type>& c); // allow multiple delete rows/columns
     double Determinant(const Matrix& m);
-
+    
     Matrix RowScale(const Matrix& m, const Matrix::size_type& r);
     Matrix RowSwap(const Matrix& m, const Matrix::size_type& r1, const Matrix::size_type& r2);
     Matrix RowReplacement(const Matrix&m, const Matrix::size_type& r1, const Matrix::size_type& r2);
     Matrix RowEchelonForm(const Matrix& m);
     Matrix ReducedRowEchelonForm(const Matrix& m);
-
-    // Matrix::size_type Rank(const Matrix& m);
-    // bool isFullRank(const Matrix& m);
-    // Matrix InverseMatrix(const Matrix & m);
+    
+    Matrix::size_type Rank(const Matrix& m);
+    bool isFullRank(const Matrix& m);
+    Matrix AugmentedMatrix(const Matrix& m1, const Matrix& m2);
+    enum class AmSide { L, R };
+    Matrix AugmentedMatrixSplit(const Matrix& m, Matrix::size_type r, AmSide side);
+    Matrix InverseMatrix(const Matrix & m);
     // Matrix Transpose(const Matrix& m);
     // bool isOrthogonal(const Matrix& m1, const Matrix& m2);
     // Vector Diagonal(const Matrix& m);
